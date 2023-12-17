170cccec3e2f589ca99c14abd86d0a054ba38539
adarnopyh <askolds.darnopihs@gmail.com>
2023-12-17 13:06:27 +0200

    3. Вызов конструктора родителя super(...) [#6837 #466685]

--- src/main/java/ru/job4j/inheritance/Profession.java
+++ src/main/java/ru/job4j/inheritance/Profession.java
@@ -3,7 +3,7 @@ package ru.job4j.inheritance;
 public class Profession {
     private boolean degree;
 
-    public Profession(boolean degree) {
+    public Profession(String programLanguage, int experience, boolean degree) {
         this.degree = degree;
     }
 }
