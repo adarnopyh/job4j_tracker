8f8b4634431abe0b8916b73ce1cf1d333f8699db
adarnopyh <askolds.darnopihs@gmail.com>
2023-12-26 12:45:29 +0200

    test

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,24 @@
+package ru.job4j.oop;
+
+public class College {
+
+    public static void main(String[] args) {
+
+        Freshman freshman = new Freshman();
+        Student student = freshman;
+        Object object = freshman;
+        Object objectFreshman = new Freshman();
+
+
+        System.out.println(freshman);
+        System.out.println(student);
+        System.out.println(object);
+        System.out.println(objectFreshman);
+        System.out.println();
+
+        System.out.println(new Freshman());
+        System.out.println(new Student());
+        System.out.println(new Object());
+
+    }
+}
