9ef4ac724b0d18437b2e0786f4006df654ceb44c
adarnopyh <askolds.darnopihs@gmail.com>
2024-01-01 16:16:00 +0200

    0.4. Пользовательские исключения. [#219367 #469853]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,11 +1,14 @@
 package ru.job4j.ex;
 
+import java.util.Objects;
+
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int result = -1;
         for (int i = 0; i < value.length; i++) {
-            if (key == value[i]) {
+            if (Objects.equals(key, value[i])) {
                 result = i;
+                break;
             }
         }
         if (result == -1) {
