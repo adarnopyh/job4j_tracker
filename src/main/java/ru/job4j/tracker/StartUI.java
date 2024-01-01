e4f9a077a72783dc3a39d2622df6472459e628ec
adarnopyh <askolds.darnopihs@gmail.com>
2024-01-01 20:11:27 +0200

    1. Обеспечить бесперебойную работу приложения Tracker [#789]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -14,6 +14,10 @@ public class StartUI {
         while (run) {
             showMenu(actions);
             int select = input.askInt("Enter menu: ");
+            if (select < 0 || select >= actions.length) {
+                output.println("Incorrect input, you can choose from 0 to " + (actions.length - 1));
+                continue;
+            }
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
@@ -28,7 +32,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new Create(output),
