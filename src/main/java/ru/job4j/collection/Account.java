5c555ab4f990de5641f7d8f4e01c88677f843bfc
adarnopyh <askolds.darnopihs@gmail.com>
2024-02-01 17:54:27 +0200

    3. Модели данных и HashSet. [#218530 #478348]

--- src/main/java/ru/job4j/collection/Account.java
+++ src/main/java/ru/job4j/collection/Account.java
@@ -38,5 +38,4 @@ public class Account {
                 + ", deposit='" + deposit + '\''
                 + '}';
     }
-
 }
--- src/main/java/ru/job4j/collection/NotifyAccount.java
+++ src/main/java/ru/job4j/collection/NotifyAccount.java
@@ -11,5 +11,4 @@ public class NotifyAccount {
         }
         return result;
     }
-
 }
