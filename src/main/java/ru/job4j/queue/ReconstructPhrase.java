612f5a8363bc51b6c93fbfc3109ede0dc8dfced8
adarnopyh <askolds.darnopihs@gmail.com>
2024-02-01 22:57:19 +0200

    2. Интерфейс Deque [#504943]

---
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -0,0 +1,38 @@
+package ru.job4j.queue;
+
+import java.util.Deque;
+
+public class ReconstructPhrase {
+
+    private final Deque<Character> descendingElements;
+
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder totalString = new StringBuilder();
+        int size = evenElements.size() / 2;
+        for (int i = 0; i < size; i++) {
+            totalString.append(evenElements.poll());
+            evenElements.poll();
+        }
+        return totalString.toString();
+    }
+
+    private String getDescendingElements() {
+        StringBuilder totalString2 = new StringBuilder();
+        int size = descendingElements.size();
+        for (int i = 0; i < size; i++) {
+            totalString2.append(descendingElements.poll());
+        }
+        return totalString2.reverse().toString();
+    }
+
+    public String getReconstructPhrase() {
+        return getEvenElements() + getDescendingElements();
+    }
+}
