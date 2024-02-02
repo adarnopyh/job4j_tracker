9023fd91b1ea2ed12b07936b1459558046ace9e6
adarnopyh <askolds.darnopihs@gmail.com>
2024-02-02 14:59:35 +0200

    1. Паспорт и жители [#212656]

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean result = true;
+        for (String key : citizens.keySet()) {
+            if (citizens.containsKey(key)) {
+                result = false;
+                break;
+            }
+        }
+        citizens.put(citizen.getPassport(), citizen);
+        return result;
+    }
+
+    public Citizen get(String passport) {
+        return citizens.get(passport);
+    }
+}
